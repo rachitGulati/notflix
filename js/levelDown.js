d97c12e6307f3c797853951cb0d3edf9686a31da
Josh Rogers <joshromanrogers@gmail.com>
2019-02-26 13:42:36 +0000

    new smaller imgs added, code formatted

--- js/levelDown.js
+++ js/levelDown.js
@@ -1,10 +1,9 @@
 let levelDown = () => {
-    if (currentArrLevel > 0) {
-      currentArrLevel--;
-      currentArr = allArrs[currentArrLevel]; 
-      chillLevelWord.innerHTML = `${chillLevelArr[currentArrLevel]}`;
-    }
-    else {
-      console.log('no lower level');
-    }
+  if (currentArrLevel > 0) {
+    currentArrLevel--;
+    currentArr = allArrs[currentArrLevel];
+    chillLevelWord.innerHTML = `${chillLevelArr[currentArrLevel]}`;
+  } else {
+    console.log('no lower level');
   }
+}
--- js/levelUp.js
+++ js/levelUp.js
@@ -1,10 +1,9 @@
 let levelUp = () => {
-    if (currentArrLevel < 2) {
-      currentArrLevel++;
-      currentArr = allArrs[currentArrLevel]; 
-      chillLevelWord.innerHTML = `${chillLevelArr[currentArrLevel]}`;
-    }
-    else {
-      console.log('no higher level');
-    }
+  if (currentArrLevel < 2) {
+    currentArrLevel++;
+    currentArr = allArrs[currentArrLevel];
+    chillLevelWord.innerHTML = `${chillLevelArr[currentArrLevel]}`;
+  } else {
+    console.log('no higher level');
   }
+}
