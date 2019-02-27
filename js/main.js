bdd54ff6679a276020d26679037414a9ab26ed77
Josh Rogers <joshromanrogers@gmail.com>
2019-02-27 13:13:25 +0000

    logo and tagline added

--- js/main.js
+++ js/main.js
@@ -9,26 +9,35 @@ class Activity {
   }
 }
 // Level 1
-let activity1 = new Activity('surfing', 'imgs/beach.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
-let activity2 = new Activity('table tennis', 'imgs/tabletennis.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
-let activity3 = new Activity('boxing', 'imgs/boxing.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
-let activity4 = new Activity('gym', 'imgs/gym.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
-let activity5 = new Activity('hiking', 'imgs/hiking.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
-let activity6 = new Activity('basketball', 'imgs/basketball.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
+let lvl1activity1 = new Activity('surfing', 'imgs/beach.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
+let lvl1activity2 = new Activity('table tennis', 'imgs/tabletennis.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
+let lvl1activity3 = new Activity('boxing', 'imgs/boxing.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
+let lvl1activity4 = new Activity('gym', 'imgs/gym.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
+let lvl1activity5 = new Activity('hiking', 'imgs/hiking.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
+let lvl1activity6 = new Activity('basketball', 'imgs/basketball.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
 
 // Level 2
-let activity7 = new Activity('meditating', 'imgs/meditate.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
-let activity8 = new Activity('sleeping', 'imgs/sleep.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
-let activity9 = new Activity('reading', 'imgs/read.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
-let activity10 = new Activity('theatre', 'imgs/theatre.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
-let activity11 = new Activity('stargazing', 'imgs/stargazing.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
-let activity12 = new Activity('library', 'imgs/library.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
+let lvl2activity1 = new Activity('meditating', 'imgs/meditate.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
+let lvl2activity2 = new Activity('sleeping', 'imgs/sleep.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
+let lvl2activity3 = new Activity('reading', 'imgs/read.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
+let lvl2activity4 = new Activity('theatre', 'imgs/theatre.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
+let lvl2activity5 = new Activity('stargazing', 'imgs/stargazing.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
+let lvl2activity6 = new Activity('library', 'imgs/library.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
 
-let arr1 = [activity1, activity2, activity3, activity4, activity5, activity6];
-let arr2 = [activity7, activity8, activity9, activity10, activity11, activity12];
+// Level 3 
+let lvl3activity1 = new Activity('meditating', 'imgs/meditate.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
+let lvl3activity2 = new Activity('sleeping', 'imgs/sleep.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
+let lvl3activity3 = new Activity('reading', 'imgs/read.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
+let lvl3activity4 = new Activity('theatre', 'imgs/theatre.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
+let lvl3activity5 = new Activity('stargazing', 'imgs/stargazing.jpg', 'google.co.uk/related', 1, ['extreme sports', 'water sports']);
+let lvl3activity6 = new Activity('library', 'imgs/library.jpg', 'google.co.uk/related', 5, ['pillow sports', 'indoors']);
+
+let arr1 = [lvl1activity1, lvl1activity2, lvl1activity3, lvl1activity4, lvl1activity5, lvl1activity6];
+let arr2 = [lvl2activity1, lvl2activity2, lvl2activity3, lvl2activity4, lvl2activity5, lvl2activity6];
+let arr3 = [lvl3activity1, lvl3activity2, lvl3activity3, lvl3activity4, lvl3activity5, lvl3activity6];
 let currentArr = arr1;
 let currentArrLevel = 1;
-let allArrs = [arr1, arr2];
+let allArrs = [arr1, arr2, arr3];
 
 let bottomImgs = ['imgs/beach.jpg', 'imgs/read.jpg', 'imgs/stargazing.jpg', 'imgs/library.jpg',
   'imgs/boxing.jpg', 'imgs/gym.jpg', 'imgs/hiking.jpg'
