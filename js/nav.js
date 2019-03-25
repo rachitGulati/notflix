88254470a948b71263738966b8e05d93f57874a1
Josh Rogers <joshromanrogers@gmail.com>
2019-03-25 18:18:53 +0000

    clean up + learn more side bar added

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+openNav = () => {
+    document.getElementById("side-nav").style.width = "400px";
+}
+
+/* Set the width of the side navigation to 0 */
+closeNav = () => {
+    document.getElementById("side-nav").style.width = "0";
+}
