8554d6e5f48411117cf958ae203a7b1cb07a6539
Josh Rogers <joshromanrogers@gmail.com>
2019-04-06 20:37:13 +0200

    side bar working properly + responsive

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,30 @@
 /* Set the width of the side navigation to 250px */
 let openNav = () => {
-	if(window.matchMedia("(max-width: 700px)").matches) {
+	if (window.matchMedia("(max-width: 500px)").matches) {
 		document.getElementById("side-nav").style.width = "100%";
-	} else {
-		document.getElementById("side-nav").style.width = "400px";
+		// document.querySelector("side-nav-p").style.width = "70vh";
+
+		document.querySelectorAll(".side-nav-p").forEach(x => x.style.width = "275px");
+	
+	} else if (window.matchMedia("(max-width: 850px)").matches) {
+		document.getElementById("side-nav").style.width = "100%";
+		// document.querySelector("side-nav-p").style.width = "70vh";
+
+		document.querySelectorAll(".side-nav-p").forEach(x => x.style.width = "450px");
+	
+	} 
+	
+	else if (window.matchMedia("(max-width: 1100px)").matches) {
+		document.getElementById("side-nav").style.width = "45%";
+		// document.querySelector("side-nav-p").style.width = "70vh";
+
+		document.querySelectorAll(".side-nav-p").forEach(x => x.style.width = "300px");
+	}
+	
+	else {
+		document.getElementById("side-nav").style.width = "45%";
+		document.querySelectorAll(".side-nav-p").forEach(x => x.style.width = "350px");
+
 	}
 };
 
@@ -12,4 +33,3 @@ let openNav = () => {
 let closeNav = () => {
 	document.getElementById("side-nav").style.width = "0";
 };
-
